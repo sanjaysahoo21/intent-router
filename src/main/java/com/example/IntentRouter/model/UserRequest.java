0ea7bb37f69ff8e9481325c74791fa4b3ed82ce6
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2026-03-11 15:10:21 +0530

    final commit

--- src/main/java/com/example/IntentRouter/model/UserRequest.java
+++ src/main/java/com/example/IntentRouter/model/UserRequest.java
@@ -7,5 +7,4 @@ public record UserRequest(
         @NotBlank(message = "Prompt must not be blank")
         @Size(max = 5000, message = "Prompt must not exceed 5000 characters")
         String prompt) {
-
 }
